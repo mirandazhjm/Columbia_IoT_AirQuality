fa61eb5c959eb3fd56e95bc02c5351ff9c4dadc1
Patrick Tsao <Patrick@dyn-160-39-203-100.dyn.columbia.edu>
2016-04-15 21:37:15 -0400

    datasource is mySQL

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var loopback = require('loopback');
 var boot = require('loopback-boot');
 
 var app = module.exports = loopback();
-var dbconfig = require('opsworks'); //[1] Include database connection data
+var dbconfig = require('./opsworks'); //[1] Include database connection data
 
 module.exports = {
   'db': {
