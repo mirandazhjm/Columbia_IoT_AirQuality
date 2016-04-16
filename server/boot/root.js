c3fc666cf138b7ff1fc5b94f8339c9cf60ddf2eb
Patrick <patrickcaonew@gmail.com>
2016-04-16 13:52:38 -0400

    Web Display Correctly Finally

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,6 +1,7 @@
+var status = require('./status');
 module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
+  router.get('/', status());
   server.use(router);
 };
